854d0691f5ac13d5065414cbce695a58ee6eb4eb
layla dantas <laylatdantas@gmail.com>
2019-06-14 21:25:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,21 +3,23 @@ class Shoe
   attr_reader :brand
 
   BRANDS = []
- def brand 
-   @brand 
- end 
-  
-  
+
+  def initialize(brand)
+    @brand = brand
+  end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
 
- 
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
+  end
 
+  BRANDS.uniq
 end
-
 
 
 
