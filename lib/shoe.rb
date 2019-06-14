c37c91dc3856498c1fdb43973cb3f25329008f78
layla dantas <laylatdantas@gmail.com>
2019-06-14 21:22:36 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,27 +4,19 @@ class Shoe
 
   BRANDS = []
 
-  def initialize(brand)
-    @brand = brand
-        BRANDS << brand
-  end
+  
+  @brand = brand
+    if !BRANDS.include?(brand)
+      BRANDS << brand
+    end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
 
-  # def brand=(brand)
-  #   @brand = brand
-  #   BRANDS << brand
-  # end
+ 
 
-  # BRANDS.uniq
-  
-  @brand = brand
-    if !BRANDS.include?(brand)
-      BRANDS << brand
-    end
 end
 
 
